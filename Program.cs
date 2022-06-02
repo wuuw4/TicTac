5b095828b3be5cf89ab3f5e2699797dc61bc2c36
sqder <sqder@DESKTOP-1JS88KI>
2022-06-03 02:26:13 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TicTac
+{
+    class Program
+    {
+        private static TicTacGame g = new TicTacGame();
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Лабораторная работа №2 Беляй Александр ИС-20-02");
+            Console.WriteLine(" Игра в Крестики-Нолики TicTacToe 3x3x3 ");
+            Console.WriteLine(" Нажмите чтобы продолжить ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(" Начнем. Ходит первый игрок. ");
+            Console.WriteLine(" Выберете ячейку, которую хотите занять, вы играете за 'X' ");
+
+            Console.WriteLine(GetPrintableState());
+
+            while(g.GetWinner() == Winner.Finished)
+            {
+                int index = int.Parse(Console.ReadLine());
+                g.MakeMove(index);
+
+                Console.WriteLine();
+                Console.WriteLine(GetPrintableState());
+
+            }
+
+
+            if (g.GetWinner() == Winner.Crosses) Console.WriteLine(" Победили крестики ");
+            if (g.GetWinner() == Winner.Zeroes) Console.WriteLine(" Победили нолики ");
+            if (g.GetWinner() == Winner.Draw) Console.WriteLine(" Ничья :# ");
+
+
+            Console.ReadLine();
+        }
+        static string GetPrintableState()
+        {
+            var sb = new StringBuilder();
+            for (int i = 1; i < 10; i += 3)
+            {
+                sb.AppendLine("     |     |     |")
+                    .AppendLine(
+                    $"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}  |")
+                    .AppendLine("_____|_____|_____|");
+            }
+            return sb.ToString();
+        }
+        static string GetPrintableChar(int index)
+        {
+            State state = g.GetState(index);
+            if(state == State.Unset)
+                return index.ToString();
+            return state == State.Cross ? "X" : "O";
+        }
+
+    }
+}
---
+++ TicTacGame.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTac
+{
+    public enum Winner
+    {
+        Crosses,
+        Zeroes,
+        Draw,
+        Finished
+    }
+    public enum State
+    {
+        Cross,
+        Zero,
+        Unset
+    }
+    public class TicTacGame
+    {
+        private readonly State[] board = new State[9];
+
+        public int MovieCounter { get; private set; }
+        //Расставить все значения доски на Unset 
+        public TicTacGame() 
+        {
+            for(int i = 0; i < board.Length; i++)
+            {
+                board[i] = State.Unset;
+            }
+        }
+        public void MakeMove(int index)
+        {
+            board[index-1] = MovieCounter % 2 == 0 ? State.Cross : State.Zero; // если ход четный - ход крестиков, в обратном случае ноликов
+
+            MovieCounter++;
+        }
+
+        public State GetState(int index)
+        {
+            return board[index - 1];
+
+        }
+
+        public Winner GetWinner()
+        {
+            return GetWinner(1, 4, 7, 2, 5, 8, 3, 6, 9, //вертикали
+                1, 2, 3, 4, 5, 6, 7, 8, 9, //горизонтали
+                1, 5, 9, 3, 5, 7); //диагонали
+        }
+
+        private Winner GetWinner(params int[] indexes)
+        {
+            for (int i = 0; i < indexes.Length; i += 3)
+            {
+                bool same = AreSame(indexes[i], indexes[i + 1], indexes[i + 2]);
+                if (same)
+                {
+                    State state = GetState(indexes[i]);
+                    if (state != State.Unset)
+                    {
+                        return state == State.Cross ? Winner.Crosses : Winner.Zeroes;
+                    }
+                }
+            }
+            if (MovieCounter < 9)
+                return Winner.Finished;
+            return Winner.Draw;
+
+        }
+
+        private bool AreSame(int a, int b, int c)
+        {
+            return GetState(a) == GetState(b) && GetState(a) == GetState(c);
+        }
+    }
+}
